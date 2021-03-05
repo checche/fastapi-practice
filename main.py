62aaacd9251ccc3ec28a560ad19ae2835d48eeb8
Takahiro Hirata <takakun47@gmail.com>
2021-03-05 15:30:46 +0900

    add: Enum model

--- main.py
+++ main.py
@@ -32,3 +32,16 @@ async def read_user_me():
 @app.get('/users/{user_id}')
 async def read_user(user_id: str):
     return {'user_id': user_id}
+
+
+@app.get('/models/{model_name}')
+async def get_model(model_name: ModelName):
+    # ModelNameのメンバとの比較
+    if model_name == ModelName.alexnet:
+        return {'model_name': model_name, 'message': 'Deep Learning FTW!'}
+
+    # 実際の値の取得
+    if model_name.value == 'lenet':
+        return {'model_name': model_name, 'message': 'LeCNN all the images'}
+
+    return {'model_name': model_name, 'message': 'Have some residuals'}
