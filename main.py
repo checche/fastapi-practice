5d736617d0e52f688c0ac36e28204609d582e02d
Takahiro Hirata <takakun47@gmail.com>
2021-03-05 15:15:17 +0900

    add: users

--- main.py
+++ main.py
@@ -23,6 +23,16 @@ async def read_item(item_id: int):
     return {'item_id': item_id}
 
 
+@app.get('/users/me')
+async def read_user_me():
+    return {'user_id': 'the current user'}
+
+
+@app.get('/users/{user_id}')
+async def read_user(user_id: str):
+    return {'user_id': user_id}
+
+
 @app.put('/items/{item_id}')
 def update_item(item_id: int, item: Item):
     return {'item_name': item.name, 'item_price': item.price, 'item_id': item_id}
