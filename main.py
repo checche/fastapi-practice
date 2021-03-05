1aa5f47a50d706b4fa3db2b789aeb27dda84720d
Takahiro Hirata <takakun47@gmail.com>
2021-03-05 16:03:00 +0900

    query parameter

--- main.py
+++ main.py
@@ -14,14 +14,28 @@ class ModelName(str, Enum):
     lenet = 'lenet'
 
 
+fake_items_db = [
+    {'item_name': 'Foo'},
+    {'item_name': 'Bar'},
+    {'item_name': 'Baz'},
+]
+
+
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
 
 
 @app.get('/items/{item_id}')
-async def read_item(item_id: int):
-    return {'item_id': item_id}
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {'item_id': item_id}
+    if q:
+        item.update({'q': q})
+    if not short:
+        item.update(
+            {'description': "This is an amazing item that has a long description"}
+        )
+    return item
 
 
 @app.get('/users/me')
