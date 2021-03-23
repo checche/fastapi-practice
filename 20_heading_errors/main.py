2f258cf0e09e49f6f3147d1a16cc5eeb2036dba2
Takahiro Hirata <takakun47@gmail.com>
2021-03-23 17:56:42 +0900

    update: 例外ハンドラの再利用

--- 20_heading_errors/main.py
+++ 20_heading_errors/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, HTTPException, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
 from fastapi.responses import JSONResponse, PlainTextResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from pydantic import BaseModel
@@ -46,17 +50,16 @@ async def read_unicorn(name: str):
 
 # StarletteのHTTPExceptionのハンドラを書くと良い。
 @app.exception_handler(StarletteHTTPException)
-async def http_exception_handler(request, exc):
-    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
+    print(f"OMG! An HTTP error!: {repr(exc)}")
+    return await http_exception_handler(request, exc)
 
 
 # validation exceptionのオーバーライド
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
-    )
+    print(f"OMG! The client sent invalid data!: {exc}")
+    return await request_validation_exception_handler(request, exc)
 
 
 @app.get("/items2/{item_id}")
