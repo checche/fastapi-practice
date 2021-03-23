fb68f4a165f94cafd9a35877ff319b0d3d9fea79
Takahiro Hirata <takakun47@gmail.com>
2021-03-23 15:35:07 +0900

    other response models

--- 15_extra_models/main.py
+++ 15_extra_models/main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
@@ -25,6 +25,40 @@ class UserInDB(UserBase):
     hashed_password: str
 
 
+class BaseItem(BaseModel):
+    description: str
+    type: str
+
+
+class CarItem(BaseItem):
+    type = "car"
+
+
+class PlaneItem(BaseItem):
+    type = "plane"
+    size: int
+
+
+class Item(BaseModel):
+    name: str
+    description: str
+
+
+items = {
+    "item1": {"description": "All my friends drive a low rider", "type": "car"},
+    "item2": {
+        "description": "Music is my aeroplane, it's my aeroplane",
+        "type": "plane",
+        "size": 5,
+    },
+}
+
+items2 = [
+    {"name": "Foo", "description": "There comes my hero"},
+    {"name": "Red", "description": "It's my aeroplane"},
+]
+
+
 def fake_password_hasher(raw_password: str):
     return "supersecret" + raw_password
 
@@ -40,3 +74,18 @@ def fake_save_user(user_in: UserIn):
 async def create_user(user_in: UserIn):
     user_saved = fake_save_user(user_in)
     return user_saved
+
+
+@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@app.get("/items/", response_model=list[Item])
+async def read_items():
+    return items2
+
+
+@app.get("/keyword-weights/", response_model=dict[str, float])
+async def read_keyword_weights():
+    return {"foo": 2.3, "bar": 3.4}
